965b33cb4301165d5288e19256035bc2f0a789fd
Ilya Solovev <srpanmf@yandex.ru>
2024-07-19 06:25:03 +1000

    Прописаны роуты

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,80 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='*' element={<NotFound404 />} />
+      <Route path='/feed' element={<Feed />} />
+
+      {/* Защищенные роуты: юзер залогинен */}
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+
+      {/* Защищенные роуты: залогиненный не должен попасть */}
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+
+      {/* Модалки */}
+      <Route
+        path='/feed/:number'
+        element={
+          <Modal
+            title={''}
+            onClose={function (): void {
+              throw new Error('Function not implemented.');
+            }}
+          >
+            <OrderInfo />{' '}
+          </Modal>
+        }
+      />
+      <Route
+        path='/ingredients/:id'
+        element={
+          <Modal
+            title={''}
+            onClose={function (): void {
+              throw new Error('Function not implemented.');
+            }}
+          >
+            <IngredientDetails />{' '}
+          </Modal>
+        }
+      />
+      {/* Защищенные модалки */}
+      <Route
+        path='/profile/orders/:number'
+        element={
+          <Modal
+            title={''}
+            onClose={function (): void {
+              throw new Error('Function not implemented.');
+            }}
+          >
+            <OrderInfo />{' '}
+          </Modal>
+        }
+      />
+    </Routes>
   </div>
 );
 
