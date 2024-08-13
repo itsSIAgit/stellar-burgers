93b3e5f0f5696cb7565128b8263e40437bf09ed2
Ilya Solovev <srpanmf@yandex.ru>
2024-08-14 02:40:03 +1000

    jest: ingredientsSlice тесты запросов

---
+++ __tests__/ingredientsSlice.test.ts
@@ -0,0 +1,44 @@
+import { expect, test, describe } from '@jest/globals';
+import { ingredientsTestData } from './testData';
+import ingredientsSliceReducer, { getIngredientsFromServer } from '../src/services/ingredientsSlice';
+
+describe('Тестируем ingredientsSlice', () => {
+  const initialState = {
+    isIngredientsError: false,
+    isIngredientsLoading: false,
+    ingredients: []
+  };
+  
+  test('[getIngredientsFromServer] Запрос ингридиентов отправлен', () => {
+    const action = { type: getIngredientsFromServer.pending.type };
+    const state = ingredientsSliceReducer(initialState, action);
+    
+    expect(state).toEqual({
+      isIngredientsError: false,
+      isIngredientsLoading: true,
+      ingredients: []
+    });
+  });
+
+  test('[getIngredientsFromServer] Запрос ингридиентов неудачен', () => {
+    const action = { type: getIngredientsFromServer.rejected.type };
+    const state = ingredientsSliceReducer(initialState, action);
+
+    expect(state).toEqual({
+      isIngredientsError: true,
+      isIngredientsLoading: false,
+      ingredients: []
+    });
+  });
+
+  test('[getIngredientsFromServer] Запрос ингридиентов успешен', () => {
+    const action = { type: getIngredientsFromServer.fulfilled.type, payload: ingredientsTestData };
+    const state = ingredientsSliceReducer(initialState, action);
+
+    expect(state).toEqual({
+      isIngredientsError: false,
+      isIngredientsLoading: false,
+      ingredients: ingredientsTestData
+    });
+  });
+});
